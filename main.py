7149596a4d90b22cb2a81ef87d73b952e150cc95
Dan Caron <dancaron@gmail.com>
2023-03-01 01:23:15 +0000

    app

--- main.py
+++ main.py
@@ -9,17 +9,19 @@ def cockcroft_gault(weight, serum_creatinine, age, gender):
 
     creatinine_clearance = ((140 - age) * weight * constant) / (72 * serum_creatinine)
 
+    # Cockcroft-Gault CrCl, mL/min = (140 – age) × (weight, kg) × (0.85 if female) / (72 × Cr, mg/dL)
+
     return creatinine_clearance
 
 # Create a Streamlit app
-def app():
-    st.title("Creatinine Clearance Calculator")
 
-    weight = st.number_input("Weight (kg)", min_value=1.0, max_value=300.0, value=70.0, step=0.1)
-    serum_creatinine = st.number_input("Serum Creatinine (umol/L)", min_value=0.1, max_value=1500.0, value=60.0, step=0.1)
-    age = st.number_input("Age (years)", min_value=1, max_value=150, value=50, step=1)
-    gender = st.selectbox("Gender", ("Male", "Female"))
+st.title("Creatinine Clearance Calculator")
 
-    if st.button("Calculate"):
-        result = cockcroft_gault(weight, serum_creatinine, age, gender)
-        st.write(f"Creatinine Clearance: {result:.2f} ml/min")
+gender = st.selectbox("Gender", ("Male", "Female"))
+age = st.number_input("Age (years)", min_value=1, max_value=150, value=50, step=1)
+weight = st.number_input("Weight (kg)", min_value=1.0, max_value=300.0, value=70.0, step=0.1)
+serum_creatinine = st.number_input("Serum Creatinine (umol/L)", min_value=0.1, max_value=1500.0, value=60.0, step=0.1)
+
+if st.button("Calculate"):
+    result = cockcroft_gault(weight, serum_creatinine, age, gender)
+    st.write(f"Creatinine Clearance: {result:.2f} ml/min")
